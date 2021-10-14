06d1378865c9bec4296e1035711ec5e18f22a431
aboffelli <arthur.boffelli@outlook.com>
2021-10-14 12:49:56 +0200

    R script re-created for the new tables. Bash script removed the '/' in the file name that was saving the file in the wrong directory

--- annotation_plots.R
+++ annotation_plots.R
@@ -5,23 +5,22 @@ library(reshape2)
 # Mac
 setwd("~/Box/Notes/Tables")
 
-fail_plot <- function(table) {
+fail_plot <- function(table, plot_name='') {
     p <- ggplot(data=melt(table), aes(x=variable, y=value)) +
-        geom_jitter(shape=21, size=1, aes(fill=variable), alpha=0.1) +
+        #geom_jitter(shape=21, size=1, aes(fill=variable), alpha=0.1) +
         geom_boxplot(aes(fill=variable), alpha=0.7) +
         theme_classic() +
-        labs(x="Filter", y="Number of variants removed") +
+        labs(x="Filter", y="Number of variants removed", title=plot_name) +
         scale_fill_manual(values=c("aquamarine", "coral", "cadetblue1", "lightpink1", "darkolivegreen1", "lightgoldenrod1", "cornflowerblue", "blueviolet", "indianred1", "lightskyblue3"))
     return(p)
 }
 
 
-plot_box <- function(table) {
+plot_box <- function(table, plot_name='') {
     p <- ggplot(melt(table), aes(x=variable, y=value)) +
         geom_boxplot(aes(fill=variable)) +
-        scale_fill_manual(values=c("cadetblue1","lightgoldenrod1", "coral","darkolivegreen1")) +
-        xlab("Group") +
-        ylab("Number of variants") +
+        scale_fill_manual(values=c("aquamarine", "coral", "cadetblue1", "lightpink1", "darkolivegreen1", "lightgoldenrod1", "cornflowerblue", "blueviolet")) +
+        labs(x="Group", y="Number of variants", title=plot_name) +
         theme_classic()
     return(p)
 }
@@ -30,64 +29,39 @@ plot_box <- function(table) {
 percentage <- function(table) {
     melted <- melt(table)
     p <- aggregate(melted$value, list(melted$variable), sum)
-    p <- cbind(p, Percentage=p$x/sum(p$x)*100)
+    p <- cbind(p, Percentage=as.numeric(format((p$x/sum(p$x))*100, scientific=F)))
     colnames(p) <- c("Group", "Number", "Percentage")
     return(p)
 }
 
-novel_vs_known_table <- read.table('novel_vs_known.txt', header = T, sep="\t")[,-1]
-on_vs_off_table <- read.table('on_vs_off.txt', header = T, sep="\t")[,-1]
-consequence_table <- read.table('consequence_list.txt', header=T, sep="\t")[,-1]
 
+################################################################################
+type_table <- read.table('type_comparison.txt', header = T, sep="\t")[,-1]
 
-novel_vs_known <- plot_box(novel_vs_known_table)
-print(novel_vs_known)
+type_comparison <- plot_box(type_table)
+print(type_comparison)
 ggsave("Plots/novel_vs_known.pdf", plot=novel_vs_known)
 
-on_vs_off <- plot_box(on_vs_off_table)
-print(on_vs_off)
-ggsave("Plots/on_vs_off.pdf", plot=on_vs_off)
-
-# consq <- ggplot(melt(consequence_table), aes(x=variable, y=value)) +
-#     geom_boxplot(aes(fill=variable)) +
-#     theme_classic()
-# print(consq)
-# ggsave("Plots/consequence.pdf", plot=consq)
-
-
-
-a_table <- percentage(novel_vs_known_table)
+a_table <- percentage(type_table)
 write.table(a_table, file="percentage_novel_vs_known.txt", sep="\t", row.names = F, quote = F)
 
-p_novel_vs_known <- ggplot(a_table, aes(x=Group, y=Percentage, fill=Group)) +
+p_type_table <- ggplot(a_table, aes(x=Group, y=Percentage, fill=Group)) +
     geom_bar(stat='identity') +
-    scale_fill_manual(values=c("cadetblue1","lightgoldenrod1", "coral","darkolivegreen1")) +
+    scale_fill_manual(values=c("aquamarine", "coral", "cadetblue1", "lightpink1", "darkolivegreen1", "lightgoldenrod1", "cornflowerblue", "blueviolet")) +
     theme_classic()
-print(p_novel_vs_known)
+print(p_type_table)
 ggsave("Plots/p_novel_vs_known.pdf", plot=p_novel_vs_known)
 
 
+################################################################################
+sor_known_on <- read.table('KnownOnFail_sor3_values_table.txt', sep='\t')
+sor_known_off <- read.table('KnownOffFail_sor3_values_table.txt', sep='\t')
+sor_novel_on <- read.table('NovelOnFail_sor3_values_table.txt', sep='\t')
+total <- rbind(sor_known_on, sor_novel_on)
 
-a_table2 <- percentage(on_vs_off_table)
-write.table(a_table2, file="percentage_on_vs_off.txt", sep="\t", row.names = F, quote = F)
-
-p_on_off <- ggplot(a_table2, aes(x=Group, y=Percentage, fill=Group)) +
-    geom_bar(stat='identity') +
-    scale_fill_manual(values=c("cadetblue1","lightgoldenrod1", "coral","darkolivegreen1")) +
+n_bin = 50
+ggplot(data=total, aes(x=V2)) +
+    geom_histogram(fill='gray', alpha=0.3, col='black', bins = n_bin) +
+    geom_freqpoly(bins=n_bin, aes(col=V1)) +
+    labs(x="SOR3 Value", y="Count") +
     theme_classic()
-print(p_on_off)
-ggsave("Plots/p_on_vs_off.pdf", plot=p_on_off)
-
-
-on_fail <- read.table("on_target_fail.txt", header=T, sep="\t")[,-1]
-off_fail <- read.table("off_target_fail.txt", header=T, sep="\t")[,-1]
-
-known_fail <- read.table("known_fail.txt", header=T, sep="\t")[,-1]
-novel_fail <- read.table("novel_fail.txt", header=T, sep="\t")[,-1]
-
-
-
-on_fail_plot <- fail_plot(on_fail)
-print(on_fail_plot)
-off_fail_plot <- fail_plot(off_fail)
-print(off_fail_plot)
--- filter_number_fail_script.sh
+++ filter_number_fail_script.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-for dir in *Fail; do
+for dir in KnownO*/; do
     filename=${dir##/};
     grep "SOR3" $dir/*.vcf | grep -oP "SOR=.+?;" | tr -d "SOR=;" | while read number; do
-        echo ${filename} $number | tr " " "\t" >> ${filename}_sor3_values_table.txt;
+        echo ${filename%/} $number | tr " " "\t" >> ${filename%/}_sor3_values_table.txt;
     done
 done
 
-for dir in NovelO*; do
+for dir in NovelO*/; do
     filename=${dir##/};
     grep "SOR3" $dir/*.vcf | grep -oP "SOR=.+?;" | tr -d "SOR=;" | while read number; do
-        echo ${filename} $number | tr " " "\t" >> ${filename}_sor3_values_table.txt;
+        echo ${filename%/} $number | tr " " "\t" >> ${filename%/}_sor3_values_table.txt;
     done
 done
