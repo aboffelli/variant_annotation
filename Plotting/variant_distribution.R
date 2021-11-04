096db2b5423c1ed53faf3179c8532f5c41cf97cc
aboffelli <arthur.boffelli@outlook.com>
2021-11-04 16:14:07 +0100

    Variants divided into 12 types for better plotting

--- Plotting/variant_distribution.R
+++ Plotting/variant_distribution.R
@@ -1,29 +1,29 @@
 library(ggplot2)
-library(reshape2)
-library(gridExtra)
 
 
-
-setwd("~/Box/Notes/Scripts/Plotting")
+setwd("~/Box/Notes/Tables")
 known = read.table('known_variant_distribution.txt', sep='\t')
 novel = read.table('novel_variant_distribution.txt', sep='\t')
+known$V1 <- paste(known$V1, ' (', known$V2, ')', sep='')
+novel$V1 <- paste(novel$V1, ' (', novel$V2, ')', sep='')
 
-
-print(ggplot(known, aes(x='', y=V2, fill=V1)) +
-    geom_col() +
-    geom_text(aes(label=V2, x=1.5), position = position_stack(vjust = 0.5), 
-              size = 2, angle=55) +
+y <- ggplot(known, aes(x='', y=V2, fill=V1)) +
+    geom_col(col='black', size=0.05) +
     coord_polar(theta = 'y') +
-    theme(legend.key.size  = unit(0.2, 'cm')) +
-    guides(fill=guide_legend(ncol=2)) +
-    theme_void())
+    theme(legend.key.size  = unit(0.2, 'cm'), legend. = 'Consequence') +
+    guides(fill=guide_legend(ncol=1)) +
+    labs(title="Known variants") +
+    theme_void()
+ggsave('known.pdf', y)
 
 
-print(ggplot(novel, aes(x='', y=V2, fill=V1)) +
-    geom_col() +
-    geom_text(aes(label=V2, x=1.48), position = position_stack(vjust = 0.5), 
-              size = 2, angle=55) +
+x <- ggplot(novel, aes(x='', y=V2, fill=V1)) +
+    geom_col(col='black', size=0.05) +
     coord_polar(theta = 'y') +
-    theme(legend.key.size  = unit(0.2, 'cm')) +
-    guides(fill=guide_legend(ncol=2)) +
-    theme_void())
+    theme(legend.key.size  = unit(0.2, 'cm'), legend.title = 'Consequence') +
+    guides(fill=guide_legend(ncol=1)) +
+    labs(title="Novel variants") +
+    theme_void()
+ggsave('novel.pdf', x)
+
+
--- Plotting/variant_distribution_table.py
+++ Plotting/variant_distribution_table.py
@@ -1,14 +1,44 @@
 import re
 import os
 
-variant_types_known = {}
-variant_types_novel = {}
+
+def check_type(dict_key, consequence):
+    global variant_types_known, variant_types_novel
+    """
+    Function to add the the count for the right consequence.
+
+    :param consequence: string containing the consequence of the variant
+    :return:
+    """
+    for key in dict_key.split('/'):
+        if key in consequence:
+            if exist:
+                variant_types_known[dict_key] += 1
+            else:
+                variant_types_novel[dict_key] += 1
+            return True
+
+
+variant_types_known = {'stop_gained': 0,
+                       'frameshift': 0,
+                       'splice_acceptor/donor': 0,
+                       'missense': 0,
+                       'inframe_deletion/insertion': 0,
+                       'splice_region': 0,
+                       'synonymous_variant': 0,
+                       '5_prime_UTR': 0,
+                       '3_prime_UTR': 0,
+                       'intron': 0,
+                       'intergenic': 0,
+                       'other': 0
+                       }
+variant_types_novel = variant_types_known.copy()
 
 files_directory = "CustomAnnotation/"
 list_of_files = os.listdir(files_directory)
 for file in list_of_files.copy():
     if '.vcf' in file:
-        with open(files_directory+file,'r') as vcf_file:
+        with open(files_directory + file, 'r') as vcf_file:
             for line in vcf_file:
                 if not line.startswith("#"):
                     filt = line.split()[6]
@@ -18,18 +48,17 @@ for file in list_of_files.copy():
                     exist = csq.split('|')[0]
                     transcripts = csq.split(',')[1:]
                     for transcript in transcripts:
-                        conseq = transcript.split('|')[4]
-                        if exist:
-                            if conseq not in variant_types_known:
-                                variant_types_known[conseq] = 1
-                            else:
-                                variant_types_known[conseq] += 1
+                        conseq = transcript.split('|')[6]
+                        for keys in variant_types_known:
+                            if check_type(keys, conseq):
+                                break
                         else:
-                            if conseq not in variant_types_novel:
-                                variant_types_novel[conseq] = 1
+                            if exist:
+                                variant_types_known['other'] += 1
                             else:
-                                variant_types_novel[conseq] += 1
+                                variant_types_novel['other'] += 1
 
+# TODO: Create table of distribution of genes.
 
 with open('known_variant_distribution.txt', 'w') as known, \
         open('novel_variant_distribution.txt', 'w') as novel:
