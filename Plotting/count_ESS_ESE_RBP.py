f651f0c1d9f99ed265d101a17ca396dffe911671
aboffelli <arthur.boffelli@outlook.com>
2021-12-02 15:39:15 +0100

    Generates 5 tables, 2 for ESS and ESE, and 3 with RBP

--- Plotting/count_ESS_ESE_RBP.py
+++ Plotting/count_ESS_ESE_RBP.py
@@ -13,32 +13,32 @@ import os
 import re
 
 
-def es_counter(csq_line, pos, exist):
+def es_counter(csq_line, exist):
     if exist:
         exist = 'known'
     else:
         exist = 'novel'
-    if pos not in set.union(set(ese_dict), set(ess_dict)):
-        transcript_line = csq_line.split(',')[1:]
-        found = False
-        for transcript in transcript_line:
-            transcript = transcript.split('|')
-            ese_ref, ese_alt, ess_ref, ess_alt = transcript[10:14]
 
-            # ESE check
-            ese_conseq = comparison(ese_ref, ese_alt)
-            if ese_conseq:
-                ese_dict[pos] = (ese_conseq, exist)
-                found = True
+    transcript_line = csq_line.split(',')[1:]
+    found = False
+    for transcript in transcript_line:
+        transcript = transcript.split('|')
+        ese_ref, ese_alt, ess_ref, ess_alt = transcript[10:14]
 
-            # ESS check
-            ess_conseq = comparison(ess_ref, ess_alt)
-            if ess_conseq:
-                ess_dict[pos] = (ess_conseq, exist)
-                found = True
+        # ESE check
+        ese_conseq = comparison(ese_ref, ese_alt)
+        if ese_conseq:
+            ese_count[ese_conseq][exist] += 1
+            found = True
 
-            if found:
-                break
+        # ESS check
+        ess_conseq = comparison(ess_ref, ess_alt)
+        if ess_conseq:
+            ess_count[ess_conseq][exist] += 1
+            found = True
+
+        if found:
+            break
 
 
 def comparison(reference, alteration):
@@ -69,70 +69,97 @@ def rbp_info(csq_line, pos, exist):
     else:
         exist = 'novel'
 
-    if pos not in rbp_dict:
+    if pos not in rbp_number:
         # Check if there is an rbp
         transcript_line = csq_line.split(',')[1:]
-        found = False
+        unique_rbps = set()
         for transcript in transcript_line:
             transcript = transcript.split('|')
             rbp = transcript[8]
             if rbp:
+
                 rbp = rbp.split('&')
-                rbps = []
                 for prot in rbp:
                     prot = prot.split(':')[0]
-                    rbps.append(prot)
-                rbp_dict[pos] = (rbps, exist)
+                    unique_rbps.add(prot)
+
+        if unique_rbps:
+            rbp_count[exist]['yes'] += 1
+            rbp_number[pos] = (len(unique_rbps), exist)
+            rbp_prot[pos] = (unique_rbps, exist)
+        else:
+            rbp_count[exist]['no'] += 1
+
 
 # Test
-# files_directory = r"C:\Users\Arthu\Documents\CustomAnnotation\\"
+# files_directory = "/Users/student/Box/Notes/TestData/CustomAnnotation/"
+# files_directory = r"C:\Users\Arthu\Documents\TestData\CustomAnnotation\\"
 files_directory = "CustomAnnotation/"
 list_of_files = os.listdir(files_directory)
 for file in list_of_files.copy():
     if '.vcf' not in file:
         list_of_files.remove(file)
 
-ese_dict = {}
-ess_dict = {}
-rbp_dict = {}
+pos_set = set()
 
+rbp_count = {'known': {'yes': 0, 'no': 0},
+             'novel': {'yes': 0, 'no': 0}
+             }
+rbp_prot = {}
+rbp_number = {}
+
+ese_count = {'alter': {'known': 0, 'novel': 0},
+             'create': {'known': 0, 'novel': 0},
+             'remove': {'known': 0, 'novel': 0}
+             }
+
+ess_count = {'alter': {'known': 0, 'novel': 0},
+             'create': {'known': 0, 'novel': 0},
+             'remove': {'known': 0, 'novel': 0}
+             }
+
+count = 1
 for file in list_of_files:
+    print(count)
     with open(files_directory + file, 'r') as vcf_file:
         for line in vcf_file:
             if not line.startswith("#"):
                 position = line.split()[1]
                 csq = re.search(r'CSQ=(\S+)', line).group(1)
                 existence = csq.split('|')[0]
-                es_counter(csq, position, existence)
-                #rbp_info(csq, position, existence)
+                if position not in pos_set:
+                    pos_set.add(position)
+                    es_counter(csq, existence)
+                    rbp_info(csq, position, existence)
+    count += 1
+print('Files are over')
 
-ese_count = {'alter': {'known': 0, 'novel': 0},
-             'create': {'known': 0, 'novel': 0},
-             'remove': {'known': 0, 'novel': 0}
-             }
-ess_count = ese_count.copy()
 
 with open('ese_count.txt', 'w') as ese_out, \
         open('ess_count.txt', 'w') as ess_out:
-    for position in ese_dict:
-        info = ese_dict[position]
-        ese_count[info[0]][info[1]] += 1
     for key in ese_count:
         for var_type in ese_count[key]:
             print(f"{key}\t{var_type}\t{ese_count[key][var_type]}",
                   file=ese_out)
 
-    for position in ess_dict:
-        info = ess_dict[position]
-        ess_count[info[0]][info[1]] += 1
     for key in ess_count:
         for var_type in ess_count[key]:
             print(f"{key}\t{var_type}\t{ess_count[key][var_type]}",
                   file=ess_out)
 
-# print(rbp_dict)
-# TODO:
-#  RBP:
-#   Check if there are a RBP (just yes or no)
-#   Get the protein name, to sort by frequency (unique prot in the variants).
-#   Count the number of RBPs in each variant (also unique prot).
+
+with open('rbp_count.txt', 'w') as rbpc:
+    for key in rbp_count:
+        for key2 in rbp_count[key]:
+            print(f"{key}\t{key2}\t{rbp_count[key][key2]}", file=rbpc)
+
+
+with open('rbp_protein_frequency.txt', 'w') as rbpp:
+    for key in rbp_prot:
+        for prot in rbp_prot[key][0]:
+            print(f'{prot}\t{rbp_prot[key][1]}', file=rbpp)
+
+
+with open('rbp_variant_frequency.txt', 'w') as rbpv:
+    for key in rbp_number:
+        print(f'{key}\t{rbp_number[key][0]}\t{rbp_number[key][1]}', file=rbpv)
