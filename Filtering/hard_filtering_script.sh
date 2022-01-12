72d081ae5940b9c40921c69a74544d60feb9dbea
aboffelli <arthur.boffelli@outlook.com>
2022-01-12 15:58:55 +0100

    Changed all SNPs to SNVs

--- Filtering/hard_filtering_script.sh
+++ Filtering/hard_filtering_script.sh
@@ -5,18 +5,18 @@
 # Create the new directories to store the results.
 mkdir HardFiltering
 cd HardFiltering
-mkdir SNPs
+mkdir SNVs
 mkdir Indels
 
-# Separate the indels from the snps using the option SelectVariants from GATK.
+# Separate the indels from the snvs using the option SelectVariants from GATK.
 for file in ../Data/*.vcf; do
-  gatk SelectVariants -V $file -select-type SNP -O SNPs/snps_${file##*/} 2>>SNPs/snps_error;
+  gatk SelectVariants -V $file -select-type SNP -O SNVs/snvs_${file##*/} 2>>SNVs/snvs_error;
   gatk SelectVariants -V $file -select-type INDEL -select-type MIXED -O Indels/indels_${file##*/} 2>>Indels/indels_error;
 done
 
 # Filter SNVs using the values sugested by the GATK best practices. With the exception of SOR, which we use > 4.0 instead of 3.0. Save the results in a new directory.
-mkdir FilteredSNPs
-for file in SNPs/snps_*.vcf; do
+mkdir FilteredSNVs
+for file in SNVs/snvs_*.vcf; do
   gatk VariantFiltration -V $file \
   -filter "QD < 2.0 " --filter-name QD2S \
   -filter "MQ < 40.0" --filter-name MQ40 \
@@ -24,7 +24,7 @@ for file in SNPs/snps_*.vcf; do
   -filter "SOR > 4.0" --filter-name SOR4 \
   -filter "MQRankSum < -12.5" --filter-name MQRS-12.5 \
   -filter "ReadPosRankSum < -8.0" --filter-name RPRS-8 \
-  -O FilteredSNPs/filtered_${file##*/} 2>>FilteredSNPs/snps_filter_error;
+  -O FilteredSNVs/filtered_${file##*/} 2>>FilteredSNVs/snvs_filter_error;
  done
 
 # Filter Indels using the values sugested by the GATK best practices. Save the results in a new directory.
@@ -41,5 +41,5 @@ done
 # Merge the Indels and SNVs together and store it in a new directory.
 mkdir Merged
 for file in FilteredIndels/*.vcf; do
-  fname=${file#*indels_}; gatk MergeVcfs -I $file -I FilteredSNPs/filtered_snps_$fname -O Merged/filtered_$fname 2>>Merged/merge_error;
+  fname=${file#*indels_}; gatk MergeVcfs -I $file -I FilteredSNVs/filtered_snvs_$fname -O Merged/filtered_$fname 2>>Merged/merge_error;
 done
