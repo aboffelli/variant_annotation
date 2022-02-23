ca9d997ea1446afcca8e4f90d24d3ebc30267e68
aboffelli <arthur.boffelli@outlook.com>
2022-02-23 17:52:56 +0100

    Updated the column numbers and added the alternative base to the position

--- BridgesScripts/Plotting/synonymous_comparison.py
+++ BridgesScripts/Plotting/synonymous_comparison.py
@@ -25,7 +25,7 @@ start_time = time.time()
 # list_of_files = glob.glob("/Users/student/Box/Notes/TestData/Bridges/**/*.vcf",
 #                           recursive=True)
 # for file in list_of_files.copy():
-#     if '/encode_vep' not in file:
+#     if '/vep' not in file:
 #         list_of_files.remove(file)
 
 # Original files
@@ -37,12 +37,15 @@ for file in list_of_files.copy():
 # Retrieve the variant positions from the synonymous table
 swea_synonymous = {}
 unique_genes = set()
+
+print('Loading synonymous_table...')
 with open('/home/ar7343bo-s/SWEA/synonymous_table.txt', 'r') as syn_table:
     header = syn_table.readline()
     for line in syn_table:
         split_line = line.strip().split('\t')
-        position = split_line[0]
+        position = ''.join(split_line[0:2])
         swea_synonymous[position] = split_line
+print('Done.\nReading files...')
 
 bridges_af = {'Control': {}, 'Samples': {}}
 bridges_af_perc = {'Control': {}, 'Samples': {}}
@@ -51,14 +54,14 @@ file_count = 1
 for file in list_of_files:
     print(file_count)
     file_type = file.split('/')[1]
-    print(file_type)
     sample_name = file.split('/')[-1].lstrip('encode_vep_')
-    print(sample_name)
     with open(file, 'r') as vcf:
         for vcfline in vcf:
             if not vcfline.startswith('#'):
                 split_line = vcfline.split('\t')
                 position = ':'.join(split_line[0:2]).lstrip('chr')
+                alt_base = vcfline.split('\t')[4]
+                position += alt_base
 
                 csq = re.search(r'CSQ=(\S+)', vcfline).group(1)
                 csq = csq.split(',')
@@ -72,10 +75,10 @@ for file in list_of_files:
                     allele = vcfline.split('\t')[-1][0:3]
                     if position not in bridges_af[file_type]:
                         bridges_af[file_type][position] = {}
-                    if allele == '0/1':  # only one allele
-                        allele = 0.5
-                    else:  # 1/1 two alleles
+                    if allele == '1/1':  # homozygous - two alleles
                         allele = 1
+                    else:  # heterozygous - one allele (0/1, 1/0 or 1/2)
+                        allele = 0.5
                     bridges_af[file_type][position][sample_name] = allele
 
     file_count += 1
@@ -90,22 +93,23 @@ for dict_type in bridges_af:
 
         # Divide the sum by the number of samples and store it in the dictionary.
         bridges_af_perc[dict_type][position] = \
-            f'{bridges_af_perc[dict_type][position] / len(bridges_af[dict_type][position]):.3f} ' \
-            f'({len(bridges_af[dict_type][position])} samples)'
+            f"""{bridges_af_perc[dict_type][position] /
+                 len(bridges_af[dict_type][position]):.3f} ({
+            len(bridges_af[dict_type][position])} samples)"""
 
 
 new_header = header.split('\t')
-new_header.insert(6, 'AF_BRIDGES_Control')
-new_header.insert(7, 'AF_BRIDGES_Samples')
+new_header.insert(7, 'AF_BRIDGES_Control')
+new_header.insert(8, 'AF_BRIDGES_Samples')
 new_header = '\t'.join(new_header).strip()
 
-with open('new_synonymous_table.txt', 'w') as outfile:
+with open('bridges_synonymous_table.txt', 'w') as outfile:
     print(new_header, file=outfile)
     for dict_type in bridges_af_perc:
         if dict_type == 'Control':
-            insert_position = 6
-        else:
             insert_position = 7
+        else:
+            insert_position = 8
         for position in swea_synonymous:
             if position in bridges_af_perc[dict_type]:
                 new_line = swea_synonymous[position]
@@ -119,9 +123,10 @@ with open('new_synonymous_table.txt', 'w') as outfile:
             if dict_type == 'Samples':
                 print('\t'.join(new_line), file=outfile)
 
-with open('bridges_gene_list.txt', 'w') as gene_list:
+with open('bridges_gene_list.txt',
+          'w') as gene_list:
     for gene_name in sorted(list(unique_genes)):
         print(gene_name, file=gene_list)
-        pass
+
 # Print the run time.
 print('Run time: {:.2f} seconds'.format(time.time() - start_time))
