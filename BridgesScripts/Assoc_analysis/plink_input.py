3130b81abdd8be974ac3e0a54baa0e7afc79f7f7
aboffelli <arthur.boffelli@outlook.com>
2022-04-12 09:50:45 +0200

    Title of script

--- BridgesScripts/Assoc_analysis/plink_input.py
+++ BridgesScripts/Assoc_analysis/plink_input.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Title:
+Title: Plink input files.
 
 Description: Script that creates two input files for PLINK.
     One tab delimited MAP file containing the information for each variant, i.e.
