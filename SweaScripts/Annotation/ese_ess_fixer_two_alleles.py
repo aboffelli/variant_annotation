2f5b86d69f6445f8cdff6daa99e72945ed99ada0
aboffelli <arthur.boffelli@outlook.com>
2022-04-28 16:44:01 +0200

    Added comments

--- SweaScripts/Annotation/ese_ess_fixer_two_alleles.py
+++ SweaScripts/Annotation/ese_ess_fixer_two_alleles.py
@@ -1,16 +1,17 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Title:
+Title: ESE/ESS fixer
 
-Description:
+Description: Script to fix the ESE/ESS hexamer overlap when a variant has 2
+    different alternative alleles.
 
 Created on: 2022-02-21
 Author: Arthur Boffelli Castro
 
-GitHub: https://github.com/aboffelli/
+GitHub: https://github.com/aboffelli/variant_annotation
 """
-# TODO: Add comments
+
 
 import os
 import re
@@ -20,25 +21,52 @@ start_time = time.time()
 
 
 def two_alleles(vcf_line, alt_bases_list):
+    """
+    Function to separate the alleles and check for overlapping ESE/ESS hexamers.
 
+    :param str vcf_line:
+    :param list alt_bases_list:
+    :return: str vcf line containing the overlapping hexamers.
+    """
+
+    # Isolate the CSQ section from the line.
     csq = re.search(r'CSQ=(\S+?),(\S+?)(;ClinVar|\s)', vcf_line)
+
+    # Isolate the transcripts.
     transcripts = csq.group(2).split(',')
+
+    # Loop through the transcripts.
     for index, transcript in enumerate(transcripts):
         transcript = transcript.split('|')
+
+        # Retrieve the exon and strand information
         exon = transcript[4]
         strand = transcript[3]
+
+        # If the variant is in an exon
         if exon:
+
+            # Start the sets to store the overlapping sequences.
             ref_motifs_ese = set()
             ref_motifs_ess = set()
             alt_motifs_ese = set()
             alt_motifs_ess = set()
+
+            # Retrieve the flanking sequences
             flanking_seq = re.search(r"FSEQ=(\S[A-Z]+\[.+\/.+\][A-Z]+)",
                                      vcfline).group(1)
-            # Retrieve the flanking sequences
+
+            # Get only 5 bases from each side of the base in question.
             seq = re.search(r'(\w{5})\[(\S+)\/\S+\](\w{5})',
                             flanking_seq)
+
+            # Join the reference sequence using the reference base.
             ref_seq = ''.join(seq.group(1, 2, 3))
+
+            # Loop through the alternative bases.
             for base in alt_bases_list:
+
+                # Join the sequence using the alternative base.
                 alt_seq = f'{seq.group(1)}{base}{seq.group(3)}'
 
                 # Check if the variant is located in the reverse strand
@@ -56,7 +84,7 @@ def two_alleles(vcf_line, alt_bases_list):
                     if hexamer in ess_set:
                         ref_motifs_ess.add(hexamer)
 
-                # Check all possible hexamers in the altered sequence
+                # Check all possible hexamers in the alternative sequence
                 # and if they are present in the the ESE and ESS sets.
                 for i in range(len(alt_seq) - 5):
                     hexamer = alt_seq[i:i + 6]
@@ -65,15 +93,26 @@ def two_alleles(vcf_line, alt_bases_list):
                     if hexamer in ess_set:
                         alt_motifs_ess.add(hexamer)
 
+            # Join the hexamers and insert in the right position in the
+            # transcript.
             transcript[10] = ';'.join(ref_motifs_ese)
             transcript[11] = ';'.join(alt_motifs_ese)
             transcript[12] = ';'.join(ref_motifs_ess)
             transcript[13] = ';'.join(alt_motifs_ess)
+
+        # Join all the transcript.
         transcripts[index] = '|'.join(transcript)
+
+    # Join all transcripts.
     transcripts = ','.join(transcripts)
+
+    # Divide the line in two using the csq section to split.
     new_vcfline = vcf_line.split(csq.group(2))
+
+    # Add the new csq section and join the line again
     new_vcfline.insert(1, transcripts)
     new_vcfline = ''.join(new_vcfline)
+
     return new_vcfline
 
 
@@ -81,19 +120,20 @@ def reverse_complement(sequence):
     """
     Function that creates the reverse complement of a DNA sequence.
 
-    :param sequence: sequence of DNA
-    :return: reverse complement
+    :param str sequence: sequence of DNA
+    :return: string with the reverse complement
     """
     # Complement dictionary
     complement_dictionary = {'A': 'T', 'C': 'G',
                              'T': 'A', 'G': 'C'}
+
     # Reverse the sequence and get the complementary base from the dictionary.
     rev_comp = ''.join(
         complement_dictionary.get(base, base) for base in reversed(sequence))
     return rev_comp
 
 
-# Load the ESS and ESE hexamers from the tables
+# Load the ESS and ESE hexamers from the tables and store them in sets.
 ese_file = '/home/ar7343bo-s/Resources/RESCUE-ESE_hexamers_200703.txt'
 ess_file = '/home/ar7343bo-s/Resources/ESS_hexamers_200824.txt'
 ese_set = set()
@@ -107,7 +147,7 @@ with open(ese_file) as ese, open(ess_file) as ess:
         ess_set.add(line.strip())
 
 
-# Put all files in a list, removing anything that is not a vcf file.
+# Prepare the files that will be used, removing anything that is not a vcf file.
 files_directory = r"ClinvarMMSplice/"
 list_of_files = os.listdir(files_directory)
 for file in list_of_files.copy():
@@ -119,26 +159,54 @@ file_count = 1
 for file in list_of_files:
     print(f"{file_count}/{len(list_of_files)}", end='\r', flush=True)
 
+    # Open the vcf file and the new file that we are writing.
     with open(files_directory + file, 'r') as vcf, open('FixedESEESS/'+file,
                                                         'w') as outfile:
+
         for vcfline in vcf:
+
+            # If it's not a header file.
             if not vcfline.startswith('#'):
+
+                # Since we are only changing the variants that have two
+                # different alternative alleles, first we check if there is a 2
+                # in the alleles.
                 split_line = vcfline.split('\t')
                 allele = split_line[-1][0:3]
 
                 if '2' in allele:
+
+                    # Get the alternative bases
                     alt_bases = split_line[4].split(',')
                     is_snv = False
+
+                    # Check if it both variants are not indels.
                     base_sum_1 = len(split_line[3] + alt_bases[0])
                     base_sum_2 = len(split_line[3] + alt_bases[1])
                     if base_sum_1 == 2 and base_sum_2 == 2:
                         is_snv = True
+
+                    # If both are snvs, call the function to find overlapping
+                    # ESE/ESS and print the new line.
                     if is_snv:
                         new_line = two_alleles(vcfline, alt_bases)
                         print(new_line, file=outfile, end='')
+
+                    # If indel, print the line directly in the output.
+                    else:
+                        print(vcfline, file=outfile, end='')
+
+                # If there is only one alternative allele, print the line
+                # directly in the output file.
                 else:
                     print(vcfline, file=outfile, end='')
+
+            # Header files are directly saved in the output file.
             else:
                 print(vcfline, file=outfile, end='')
 
     file_count += 1
+
+print("\nDone!")
+
+print('\nRun time: {:.2f} seconds'.format(time.time() - start_time))
--- SweaScripts/Annotation/variant_position_parser.py
+++ SweaScripts/Annotation/variant_position_parser.py
@@ -11,11 +11,16 @@ Author: Arthur Boffelli Castro
 
 GitHub: https://github.com/aboffelli/variant_annotation
 """
-# TODO: Add comments.
+
 import os
 import sys
+import re
+import time
 
-# Dictionary with the flanking region size used in the sequencing.
+start_time = time.time()
+
+# Create a dictionary with the flanking region size used in the sequencing for
+# the
 flanking_regions = {'BRCA1': 150000, 'BRCA2': 150000, 'CDH1': 50000,
                     'CHEK2': 50000, 'PTEN': 50000, 'STK11': 50000,
                     'TP53': 50000, 'ATM': 20000, 'BARD1': 20000,
@@ -24,7 +29,10 @@ flanking_regions = {'BRCA1': 150000, 'BRCA2': 150000, 'CDH1': 50000,
                     'RAD51C': 20000, 'RAD51D': 20000
                     }
 
-# Retrieve the positions of the genes that where sequenced
+# Retrieve the positions of the genes that where sequenced. The first argument
+# in the command line should be the gff file with the positions of the genes,
+# and the second argument should be a file with all the genes targeted in the
+# sequencing listed.
 if len(sys.argv) >= 3:
     gff = sys.argv[1]
     whole_genes = sys.argv[2]
@@ -40,34 +48,66 @@ if len(sys.argv) >= 3:
 
         # Loop through the gff file
         for gff_line in resource:
+
+            # Ignore comment lines.
             if not gff_line.startswith("#"):
                 gff_line = gff_line.strip().split('\t')
+
+                # Get only the gene lines.
                 if gff_line[2] == "gene":
-                    gene_name = gff_line[8].split(';')[3].strip('gene_name=')
+
+                    # The gene name is located in the 9th column, after
+                    # gene_name.
+                    gene_name = re.search(r"gene_name=(\w+)",
+                                          gff_line[8]).group(1)
+
+                    # Loop through the list of targeted genes.
                     for gene in gl:
+
+                        # If the same gene.
                         if gene == gene_name:
+
+                            # Get the chromosome number.
                             chromosome = gff_line[0].strip("chr")
+
+                            # Check if it is one of the genes that were
+                            # sequenced with flanking regions.
                             if gene in flanking_regions:
+                                # Store the size of the flanking region.
                                 fr = flanking_regions[gene]
                             else:
+                                # Set it to zero if there is no flanking region.
                                 fr = 0
+
+                            # Create a new key in the dictionary if the
+                            # chromosome is not there yet.
                             if chromosome not in gene_dict:
+                                # For each chromosome, create a dictionary with
+                                # the start and end of each gene, adding the
+                                # flanking regions.
                                 gene_dict[chromosome] = {gene_name: (
                                     int(gff_line[3]) - fr,
                                     int(gff_line[4]) + fr)}
+
                                 break
 
+                            # If the chromosome already exists in the
+                            # dictionary.
                             else:
+                                # Add the new gene with the respective start and
+                                # end positions, adding the flanking regions.
                                 gene_dict[chromosome][gene_name] = (
                                     int(gff_line[3]) - fr,
                                     int(gff_line[4]) + fr)
                                 break
 
-    # Create the new directories
+    # Prepare the files that will be used, removing anything that is not a vcf.
     list_of_files = os.listdir("Annotation/Edited")
     for file in list_of_files.copy():
         if '.vcf' not in file:
             list_of_files.remove(file)
+
+    # Create the new directories for the output files.
     new_directories = ['Annotation/Edited/KnownOnFail/',
                        'Annotation/Edited/KnownOffFail/',
                        'Annotation/Edited/KnownOnPass/',
@@ -81,13 +121,13 @@ if len(sys.argv) >= 3:
         if not os.path.exists(directory):
             os.makedirs(directory)
 
-    # Start a counter for filters
+    # Start a counter for each filter
     filters = {"QD2S": 0, "MQ40": 0, "FS60": 0,
                "SOR4": 0, "MQRS-12.5": 0, "RPRS-8": 0,
                "QD2I": 0, "FS200": 0, "SOR10": 0, "RPRS-20": 0
                }
 
-    # Open count files and create the headers
+    # Open the count files and create the headers
     with open('type_comparison.txt', 'w') as out, open(
             "known_on_target_fail.txt", 'w') as k_on_fail, open(
             "known_off_target_fail.txt", "w") as k_off_fail, open(
@@ -105,6 +145,7 @@ if len(sys.argv) >= 3:
         n_on_fail.write("Sample")
         n_off_fail.write("Sample")
 
+        # Add the filter names in the header.
         for key in filters:
             k_on_fail.write("\t" + key)
             k_off_fail.write("\t" + key)
@@ -113,7 +154,8 @@ if len(sys.argv) >= 3:
 
         # Loop through the vcf files
         for file in list_of_files:
-            # For each file reset the variables
+
+            # For each file reset all the counters.
             sample = file.lstrip('fs_filtered_').rstrip(".vcf")
             k_on_fail_filters = filters.copy()
             k_off_fail_filters = filters.copy()
@@ -123,6 +165,7 @@ if len(sys.argv) >= 3:
                        "k_off_fail": 0, "n_on_pass": 0, "n_on_fail": 0,
                        "n_off_pass": 0, "n_off_fail": 0}
 
+            # Open all the output files.
             with open('Annotation/Edited/' + file, 'r') as vcf, open(
                 'Annotation/Edited/KnownOnFail/k_on_fail_' + file,
                     'w') as k_on_fail_out, open(
@@ -142,7 +185,8 @@ if len(sys.argv) >= 3:
                     'w') as n_off_pass_out:
 
                 for line in vcf:
-                    # Get the information we need of each line.
+
+                    # Get the information we need from each variant.
                     if not line.startswith("#"):
                         line_split = line.split()
                         chrom = line_split[0]
@@ -150,64 +194,115 @@ if len(sys.argv) >= 3:
                         filter_col = line_split[6]
                         exist = line_split[7].split('|')[1]
 
-                        # On or Off target
+                        # Check if the variant is on or off the targeted areas.
                         if chrom in gene_dict:
+
+                            # Loop through all the genes in our dictionary, and
+                            # store the start an end positions in variables.
                             for gene in gene_dict[chrom]:
                                 start = gene_dict[chrom][gene][0]
                                 end = gene_dict[chrom][gene][1]
-                                if start < int(pos) < end:  # On target
-                                    if exist:  # Known
-                                        if filter_col == 'PASS':  # Pass
+
+                                # If the variant position is between the start
+                                # and end of a gene, it is on target.
+                                if start <= int(pos) <= end:
+
+                                    # Check if it is a known variant.
+                                    if exist:
+
+                                        # Check if the variant passed the
+                                        # filters and add to the respective
+                                        # counter.
+                                        if filter_col == 'PASS':
                                             counter["k_on_pass"] += 1
                                             k_on_pass_out.write(line)
                                             break
                                         else:  # Fail
                                             counter["k_on_fail"] += 1
                                             k_on_fail_out.write(line)
+
+                                            # The variant may fail more than one
+                                            # filter, so make sure we count all
+                                            # of them.
                                             for f in filter_col.split(";"):
                                                 k_on_fail_filters[f] += 1
                                             break
+
                                     else:  # Novel
+
                                         if filter_col == 'PASS':  # Pass
                                             counter["n_on_pass"] += 1
                                             n_on_pass_out.write(line)
                                             break
+
                                         else:  # Fail
                                             counter["n_on_fail"] += 1
                                             n_on_fail_out.write(line)
                                             for f in filter_col.split(";"):
                                                 n_on_fail_filters[f] += 1
+
                                             break
-                            else:  # Off target
-                                if exist:  # Known
+
+                            # If the variant is not between the start and end,
+                            # it is off target.
+                            else:
+
+                                # Check if it is a known variant.
+                                if exist:
+
+                                    # Check if the variant passed the
+                                    # filters and add to the respective
+                                    # counter.
                                     if filter_col == 'PASS':  # Pass
                                         counter["k_off_pass"] += 1
                                         k_off_pass_out.write(line)
+
                                     else:  # Fail
                                         counter["k_off_fail"] += 1
                                         k_off_fail_out.write(line)
+
+                                        # The variant may fail more than one
+                                        # filter, so make sure we count all
+                                        # of them.
                                         for f in filter_col.split(";"):
                                             k_off_fail_filters[f] += 1
+
                                 else:  # Novel
+
                                     if filter_col == 'PASS':  # Pass
                                         counter["n_off_pass"] += 1
                                         n_off_pass_out.write(line)
+
                                     else:  # Fail
                                         counter["n_off_fail"] += 1
                                         n_off_fail_out.write(line)
                                         for f in filter_col.split(";"):
                                             n_off_fail_filters[f] += 1
 
-                        else:  # Chromosome that is not in the dict.
-                            if exist:  # Known
+                        # If the chromosome is not in the dictionary, it is also
+                        # off target.
+                        else:
+
+                            # Check if it is a known variant.
+                            if exist:
+
+                                # Check if the variant passed the
+                                # filters and add to the respective
+                                # counter.
                                 if filter_col == 'PASS':  # Pass
                                     counter["k_off_pass"] += 1
                                     k_off_pass_out.write(line)
+
                                 else:  # Fail
                                     counter["k_off_fail"] += 1
                                     k_off_fail_out.write(line)
+
+                                    # The variant may fail more than one
+                                    # filter, so make sure we count all
+                                    # of them.
                                     for f in filter_col.split(";"):
                                         k_off_fail_filters[f] += 1
+
                             else:  # Novel
                                 if filter_col == 'PASS':  # Pass
                                     counter["n_off_pass"] += 1
@@ -216,15 +311,18 @@ if len(sys.argv) >= 3:
                                 else:  # Fail
                                     counter["n_off_fail"] += 1
                                     n_off_fail_out.write(line)
+
                                     for f in filter_col.split(";"):
                                         n_off_fail_filters[f] += 1
 
-            # Print the values on file type_comparison.txt
+            # Print the values on file type_comparison.txt before going to the
+            # next vcf file.
             out.write("\n" + sample)
             for key in counter:
                 out.write("\t" + str(counter[key]))
 
-            # Print the values on fail output files
+            # Print the values on fail output files. Start the line with the
+            # sample name, and add the count.
             k_on_fail.write("\n" + sample)
             k_off_fail.write("\n" + sample)
             n_on_fail.write("\n" + sample)
@@ -234,6 +332,8 @@ if len(sys.argv) >= 3:
                 k_off_fail.write("\t" + str(k_off_fail_filters[key]))
                 n_on_fail.write("\t" + str(n_on_fail_filters[key]))
                 n_off_fail.write("\t" + str(n_off_fail_filters[key]))
+
+    print('\nRun time: {:.2f} seconds'.format(time.time() - start_time))
 
 else:
     print("Usage:\npython variant_position_parser.py file.gff gene_list.txt")
